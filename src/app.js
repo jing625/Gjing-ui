4473a493ba9420314d93bc43a26f4d63fff05c17
jing <244457187@qq.com>
2018-12-19 18:21:35 +0800

    开始单元测试(还有Bug)

--- src/app.js
+++ src/app.js
@@ -3,17 +3,32 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 
-Vue.component('g-button',Button)
-Vue.component('g-icon',Icon)
-Vue.component('g-button-group',ButtonGroup)
+Vue.component('g-button', Button)
+Vue.component('g-icon', Icon)
+Vue.component('g-button-group', ButtonGroup)
 new Vue({
     el: '#app',
-    data:{
-        loading1:false,
-        loading2:true,
-        loading3:false
+    data: {
+        loading1: false,
+        loading2: true,
+        loading3: false
     }
 })
 
-
-console.log(Button)
+import chai from 'chai'
+const expect = chai.expect
+//单元测试
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData:{
+            icon: 'settings'
+        }
+    })
+    console.log(button);
+    button.$mount('#test')
+    console.log(button.$el);
+    let useElement = button.$el.querySelector('use')
+    console.log(useElement);
+    expect(useElement.getAttribute('xlink:href').to.eq('#i-settings'))
+}
