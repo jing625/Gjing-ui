da386f0336413ffce3b367f577e6740ffc4dbead
lgj <469779397@qq.com>
2019-03-12 15:58:12 +0800

    col 和 row 布局进一步完善

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="col" :class="`col-${span}`">
-        <solt></solt>
+    <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+         :style="{paddingLeft: gutter/2 + 'px', paddingRight: gutter/2 + 'px'}">
+        <div style="border: 1px solid blue;">
+            <slot></slot>
+        </div>
     </div>
 </template>
 <script>
@@ -9,6 +12,14 @@
         props: {
             span: {
                 type: [Number, String]
+            },
+            offset: {
+                type: [Number, String]
+            }
+        },
+        data() {
+            return {
+                gutter: 0,
             }
         }
 
@@ -20,10 +31,17 @@
         background: grey;
         width: 50%;
         border: 1px solid red;
+        /*padding: 0 10px;*/
         $class-prefix: col-;
         @for $n from 1 through 24 {
             &.#{$class-prefix}#{$n} {
                 width: ($n/24) * 100%;
+            }
+        }
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n/24) * 100%;
             }
         }
     }
--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,24 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft: -gutter/2 + 'px',marginRight: -gutter/2 + 'px'}">
         <slot></slot>
     </div>
 </template>
+<script>
+    export default {
+        name: 'GuluRow',
+        props:{
+            gutter:{
+                type:[Number,String]
+            }
+        },
+        mounted(){
+            console.log(this.$children)
+            this.$children.forEach((vm)=>{
+                vm.gutter = this.gutter
+            })
+        }
+    }
+</script>
 <style lang="scss" scoped>
     .row {
         display: flex;
