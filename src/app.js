c37ad8e983874781041ba85c362c04650dca0368
lgj <469779397@qq.com>
2019-03-12 11:20:25 +0800

    col 和 row 布局初步完成

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
+import Col from './col'
+import Row from './row'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input', Input)
+Vue.component('g-col', Col)
+Vue.component('g-row', Row)
 new Vue({
     el: '#app',
     data: {
