6252f025e6b875913ab4ce06058b6a4df6c2f996
lgj <469779397@qq.com>
2019-03-13 14:43:55 +0800

    响应式基本完成

--- src/col.vue
+++ src/col.vue
@@ -40,10 +40,10 @@
                 return [
                     span && `col-${span}`,
                     offset && `offset-${offset}`,
-                    ... (ipad && [`col-ipad-${ipad.span}`]),
-                    ... (narrowPc && [`col-narrow-pc-${narrowPc.span}`]),
-                    ... (pc && [`col-pc-${pc.span}`]),
-                    ... (widePc && [`col-wide-pc-${widePc.span}`]),
+                    ... (ipad ? [`col-ipad-${ipad.span}`] : []),
+                    ... (narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
+                    ... (pc ? [`col-pc-${pc.span}`] : []),
+                    ... (widePc ? [`col-wide-pc-${widePc.span}`] : []),
                 ]
             },
             colStyle() {
@@ -75,7 +75,7 @@
             }
         }
 
-        @media (min-width: 577px) and (max-width: 768px) {
+        @media (min-width: 577px) {
             $class-prefix: col-ipad-;
             @for $n from 1 through 24 {
                 &.#{$class-prefix}#{$n} {
@@ -89,7 +89,7 @@
                 }
             }
         }
-        @media (min-width: 769px) and (max-width: 992px) {
+        @media (min-width: 769px) {
             $class-prefix: col-narrow-pc-;
             @for $n from 1 through 24 {
                 &.#{$class-prefix}#{$n} {
@@ -103,7 +103,7 @@
                 }
             }
         }
-        @media (min-width: 993px) and (max-width: 1200px) {
+        @media (min-width: 993px) {
             $class-prefix: col-pc-;
             @for $n from 1 through 24 {
                 &.#{$class-prefix}#{$n} {
