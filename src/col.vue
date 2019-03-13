57e8d6435af2cfb8a4d6b9584a633a2b29aea6bb
lgj <469779397@qq.com>
2019-03-13 14:57:40 +0800

    响应式代码重构

--- src/col.vue
+++ src/col.vue
@@ -37,13 +37,13 @@
         computed: {
             colClass() {
                 let {span, offset, ipad, narrowPc, pc, widePc} = this
+                let createClasses = this.createClasses
                 return [
-                    span && `col-${span}`,
-                    offset && `offset-${offset}`,
-                    ... (ipad ? [`col-ipad-${ipad.span}`] : []),
-                    ... (narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
-                    ... (pc ? [`col-pc-${pc.span}`] : []),
-                    ... (widePc ? [`col-wide-pc-${widePc.span}`] : []),
+                    ...createClasses({span, offset}),
+                    ...createClasses(ipad, 'ipad-'),
+                    ...createClasses(narrowPc, 'narrow-pc-'),
+                    ...createClasses(pc, 'pc-'),
+                    ...createClasses(widePc, 'wide-pc-'),
                 ]
             },
             colStyle() {
@@ -53,7 +53,16 @@
                     paddingRight: this.gutter / 2 + 'px'
                 }
             }
-        }
+        },
+        methods: {
+            createClasses (obj, str = '') {
+                if (!obj) {return []}
+                let array = []
+                if (obj.span) { array.push(`col-${str}${obj.span}`) }
+                if (obj.offset) { array.push(`offset-${str}${obj.offset}`) }
+                return array
+            }
+        },
 
     }
 </script>
