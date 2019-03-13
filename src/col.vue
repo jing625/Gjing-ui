4a160b075dc4202a06293018dbe185329441f3bb
lgj <469779397@qq.com>
2019-03-13 14:18:35 +0800

    默认设置成 phone 样式

--- src/col.vue
+++ src/col.vue
@@ -36,12 +36,10 @@
         },
         computed: {
             colClass() {
-                let {span, offset, phone, ipad, narrowPc, pc, widePc} = this
-                let phoneClass = []
+                let {span, offset, ipad, narrowPc, pc, widePc} = this
                 return [
                     span && `col-${span}`,
                     offset && `offset-${offset}`,
-                    ... (phone && [`col-phone-${phone.span}`]),
                     ... (ipad && [`col-ipad-${ipad.span}`]),
                     ... (narrowPc && [`col-narrow-pc-${narrowPc.span}`]),
                     ... (pc && [`col-pc-${pc.span}`]),
@@ -76,20 +74,7 @@
                 margin-left: ($n/24) * 100%;
             }
         }
-        @media (max-width: 576px) {
-            $class-prefix: col-phone-;
-            @for $n from 1 through 24 {
-                &.#{$class-prefix}#{$n} {
-                    width: ($n / 24) * 100%;
-                }
-            }
-            $class-prefix: offset-phone-;
-            @for $n from 1 through 24 {
-                &.#{$class-prefix}#{$n} {
-                    margin-left: ($n / 24) * 100%;
-                }
-            }
-        }
+
         @media (min-width: 577px) and (max-width: 768px) {
             $class-prefix: col-ipad-;
             @for $n from 1 through 24 {
