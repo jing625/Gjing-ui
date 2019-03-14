bac53250de71fc5a0f456a4c2fd0f94a7b51ee4c
lgj <469779397@qq.com>
2019-03-14 11:13:47 +0800

    add toast and plugin

---
+++ src/toast.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="toast">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'GuluToast'
+    }
+</script>
+<style lang="scss" scoped>
+    $font-size: 14px;
+    $toast-height: 40px;
+    $toast-bg: rgba(0, 0, 0, 0.75);
+    .toast {
+        font-size: $font-size;
+        height: $toast-height;
+        line-height: 1.8;
+        position: fixed;
+        left: 50%;
+        top: 0;
+        transform: translateX(-50%);
+        display: flex;
+        color: white;
+        align-items: center;
+        background: $toast-bg;
+        border-radius: 4px;
+        box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50);
+        padding: 0 16px;
+    }
+</style>
