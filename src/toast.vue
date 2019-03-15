ec5e9622a5c89c40b961585348313f1291d3589b
lgj <469779397@qq.com>
2019-03-15 11:03:41 +0800

    完成 toast 测试用例

--- src/toast.vue
+++ src/toast.vue
@@ -15,12 +15,11 @@
         name: 'GuluToast',
         props: {
             autoClose: {
-                type: Boolean,
-                default: true,
-            },
-            autoCloseDelay: {
-                type: Number,
-                default: 50
+                type: [Boolean,Number],
+                default: 5,
+                validator(value){
+                    return value === false || typeof value === 'number'
+                }
             },
             closeButton: {
                 type: Object,
@@ -68,7 +67,7 @@
                 if (this.autoClose) {
                     setTimeout(() => {
                         this.close()
-                    }, this.autoCloseDelay * 1000)
+                    }, this.autoClose * 1000)
                 }
             },
             close() {
