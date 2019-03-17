ad4b74b5782255938541954c03bcff66abed7e7c
lgj <469779397@qq.com>
2019-03-17 23:18:47 +0800

    解决当容器有 overflow hidden 造成的 bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="popover" @click.stop="xxx">
-        <div class="content-wrapper" v-if="visible" @click.stop>
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
             <slot name="content"></slot>
         </div>
-        <slot></slot>
+        <span ref="triggerWrapper">
+            <slot></slot>
+        </span>
     </div>
 </template>
 
@@ -15,11 +17,18 @@
                 visible: false
             }
         },
+        mounted() {
+
+        },
         methods: {
             xxx() {
                 this.visible = !this.visible
                 if (this.visible === true) {
                     this.$nextTick(() => {
+                        document.body.appendChild(this.$refs.contentWrapper)
+                        let {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect()
+                        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+                        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
                         let eventHandler = () => {
                             this.visible = false
                             document.removeEventListener('click', eventHandler)
@@ -37,13 +46,13 @@
     .popover {
         display: inline-block;
         vertical-align: top;
-        position: relative;
-        > .content-wrapper {
-            position: absolute;
-            bottom: 100%;
-            left: 0;
-            border: 1px solid red;
-            box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-        }
+        /*position: relative;*/
+
+    }
+    .content-wrapper {
+        position: absolute;
+        border: 1px solid red;
+        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+        transform: translateY(-100%);
     }
 </style>
