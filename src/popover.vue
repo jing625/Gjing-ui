82c77d7828a388382089b1b6a28b67fea1e4263a
lgj <469779397@qq.com>
2019-03-18 16:35:52 +0800

    功能方面基本完成

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="popover" @click.stop="xxx">
-        <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
+    <div class="popover" @click="onClick">
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
         <span ref="triggerWrapper">
@@ -21,23 +21,58 @@
 
         },
         methods: {
-            xxx() {
-                this.visible = !this.visible
-                if (this.visible === true) {
-                    this.$nextTick(() => {
-                        document.body.appendChild(this.$refs.contentWrapper)
-                        let {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect()
-                        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-                        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
-                        let eventHandler = () => {
-                            this.visible = false
-                            document.removeEventListener('click', eventHandler)
-                        }
-                        document.addEventListener('click', eventHandler)
-                    })
-
+            positionContent() {
+                document.body.appendChild(this.$refs.contentWrapper)
+                let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+                this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+                this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+            },
+            onClickDocument(e) {
+                if (this.$refs.contentWrapper &&
+                    (this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target))
+                ) {
+                    return
+                }
+                this.close()
+            },
+            open() {
+                this.visible = true
+                this.$nextTick(() => {
+                    this.positionContent()
+                    document.addEventListener('click', this.onClickDocument)
+                })
+            },
+            close() {
+                this.visible = false
+                document.removeEventListener('click', this.onClickDocument)
+            },
+            onClick(event) {
+                if (this.$refs.triggerWrapper.contains(event.target)) {
+                    if (this.visible === true) {
+                        this.close()
+                    } else {
+                        this.open()
+                    }
                 }
             }
+
+            // onClick() {
+            //     this.visible = !this.visible
+            //     if (this.visible === true) {
+            //         this.$nextTick(() => {
+            //             document.body.appendChild(this.$refs.contentWrapper)
+            //             let {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect()
+            //             this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+            //             this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+            //             let eventHandler = () => {
+            //                 this.visible = false
+            //                 document.removeEventListener('click', eventHandler)
+            //             }
+            //             document.addEventListener('click', eventHandler)
+            //         })
+            //
+            //     }
+            // }
         },
     }
 </script>
@@ -49,6 +84,7 @@
         /*position: relative;*/
 
     }
+
     .content-wrapper {
         position: absolute;
         border: 1px solid red;
