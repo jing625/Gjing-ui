632ca31c76dd7a6fca229377d0b2a71fbb715ab1
lgj <469779397@qq.com>
2019-03-18 16:44:34 +0800

    重构 tabs.vue

--- src/tabs.vue
+++ src/tabs.vue
@@ -31,20 +31,28 @@
                 eventBus: this.eventBus
             }
         },
-        mounted() {
-            if (this.$children.length === 0) {
-                console && console.warn &&
-                console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
-            }
-            this.$children.forEach((vm) => {
-                if (vm.$options.name === 'GuluTabsHead') {  //第一次遍历找到tabsHead
-                    vm.$children.forEach((childVm) => {
-                        if (childVm.$options.name === 'GuluTabsItem' && childVm.name === this.selected) { //第二次遍历找到tabsItem 且找到被选中的item
-                            this.eventBus.$emit('update:selected', this.selected,childVm)
-                        }
-                    })
+        methods: {
+            checkChildren() {
+                if (this.$children.length === 0) {
+                    console && console.warn &&
+                    console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
                 }
-            })
+            },
+            selectTab() {
+                this.$children.forEach((vm) => {
+                    if (vm.$options.name === 'GuluTabsHead') {  //第一次遍历找到tabsHead
+                        vm.$children.forEach((childVm) => {
+                            if (childVm.$options.name === 'GuluTabsItem' && childVm.name === this.selected) { //第二次遍历找到tabsItem 且找到被选中的item
+                                this.eventBus.$emit('update:selected', this.selected, childVm)
+                            }
+                        })
+                    }
+                })
+            }
+        },
+        mounted() {
+            this.checkChildren()
+            this.selectTab()
         }
     }
 </script>
