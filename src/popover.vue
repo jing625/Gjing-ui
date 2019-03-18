38ef5f169a971e4cef52a96991178451a7674efe
lgj <469779397@qq.com>
2019-03-18 17:47:15 +0800

    添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
         <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
-        <span ref="triggerWrapper">
+        <span ref="triggerWrapper" class="button-wrapper">
             <slot></slot>
         </span>
     </div>
@@ -28,11 +28,12 @@
                 this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
             },
             onClickDocument(e) {
+                if (this.$refs.popover &&
+                    (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
+                ) { return }
                 if (this.$refs.contentWrapper &&
                     (this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target))
-                ) {
-                    return
-                }
+                ) { return }
                 this.close()
             },
             open() {
@@ -78,17 +79,48 @@
 </script>
 
 <style lang="scss" scoped>
+    $border-color: #333;
+    $border-radius: 4px;
     .popover {
         display: inline-block;
         vertical-align: top;
-        /*position: relative;*/
-
+        position: relative;
+        > .button-wrapper {
+            display: inline-block;
+        }
     }
 
     .content-wrapper {
         position: absolute;
-        border: 1px solid red;
-        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+        border: 1px solid $border-color;
+        border-radius: $border-radius;
+        /*box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);*/
+        filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
+        background: white;
         transform: translateY(-100%);
+        margin-top: -10px;
+        padding: .5em 1em;
+        max-width: 20em;
+        word-break: break-all;
+        &::before, &::after {
+            display: block;
+            content: '';
+            border: 10px solid transparent;
+            height: 0;
+            width: 0;
+            position: absolute;
+            left: 10px;
+        }
+        &::before {
+            border-top: 10px solid black;
+            top: 100%;
+        }
+        &::after {
+            border-top: 10px solid white;
+            top: calc(100% - 1px);
+    ;
     }
+
+    }
+
 </style>
