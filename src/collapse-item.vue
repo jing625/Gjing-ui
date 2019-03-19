4de96a32f2ad4d22a6ab41362a7d989b0870834a
lgj <469779397@qq.com>
2019-03-19 11:18:59 +0800

    添加 single 选项

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="collapseItem" @click="open=!open">
+    <div class="collapseItem" @click="toggle">
         <div class="title">
             {{title}}
         </div>
@@ -12,6 +12,7 @@
 <script>
     export default {
         name: "GuluCollapse-item",
+        inject: ['eventBus'],
         props: {
             title: {
                 type: String,
@@ -23,6 +24,26 @@
                 open: false
             }
         },
+        mounted(){
+           this.eventBus && this.eventBus.$on('update:selected',(vm)=>{
+                if(vm !== this){
+                    this.close()
+                }
+            })
+        },
+        methods:{
+            toggle(){
+                if(this.open){
+                    this.open = false
+                } else {
+                    this.open = true
+                    this.eventBus && this.eventBus.$emit('update:selected',this)
+                }
+            },
+            close(){
+                this.open = false
+            }
+        }
     }
 </script>
 
