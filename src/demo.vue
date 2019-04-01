db2f5d6b491d7fd3f05e12529065abdf7400ca89
lgj <469779397@qq.com>
2019-04-01 23:04:57 +0800

    改用 vue-cli 构建项目

---
+++ src/demo.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        hi
+        <g-button>1121</g-button>
+    </div>
+</template>
+
+<script>
+    import Button from './button/button'
+
+    export default {
+        name: "demo",
+        components: {
+            'g-button': Button
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :root {
+        --font-size: 14px;
+    }
+
+    body {
+        font-size: var(--font-size)
+    }
+
+    #app {
+        /*padding: 100px;*/
+    }
+</style>
